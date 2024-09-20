6ac49bd0afb7fd7bf43c61c7c81715ed523d930c
nova <oliverquirk11@gmail.com>
2024-09-20 17:57:03 +0100

    Fixed an issue where the side menu wouldnt close when a nav button was clicked on

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from '../styles/NavButton.module.scss'
 
-const NavButton = ({text, href, onCLick}) => {
+const NavButton = ({text, href, onClick}) => {
     return (
-        <Link className={styles.navLink} to={href} onClick={onCLick}>{text}</Link>
+        <Link className={styles.navLink} to={href} onClick={onClick}>{text}</Link>
     );
 }
 
