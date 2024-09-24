9566c72616b089a2bc9eab034b02a6fbdd27e9cf
nova <oliverquirk11@gmail.com>
2024-09-24 17:25:13 +0100

    added navigation to nv1 home

--- src/pages/Nv1.js
+++ src/pages/Nv1.js
@@ -9,7 +9,8 @@ import support from '../assets/images/nv1/roleIcons/Support icon@1x.png';
 import tank from '../assets/images/nv1/roleIcons/Tank icon@1x.png';
 
 const Nv1 = () => {
-    const [session, setSession] = useState(null)
+    const [session, setSession] = useState(null);
+    const [trackGames, setTrackGames] = useState(false);
 
     useEffect(() => {
         supabase.auth.getSession().then(({ data: { session } }) => {
@@ -25,6 +26,14 @@ const Nv1 = () => {
         return () => subscription.unsubscribe()
     }, [])
 
+    const trackGame = () => {
+        setTrackGames(true);
+    }
+
+    const trackGameBack = () => {
+        setTrackGames(false);
+    }
+
     if(!session) {
         return (
             <div>
@@ -35,12 +44,29 @@ const Nv1 = () => {
                 </div>
             </div>
         )
-    } else {
+    } else if (!trackGames) {
         return (
             <div>
-                <PageTitle text='Overwatch tools' />
+                <PageTitle text='Overwatch Tools'/>
                 <div className={styles.pageContent}>
-                    <p className={styles.selectRoleText}>Select a role you would like to track games for:</p>
+                    <p className={styles.text}>Press the track games button to track a new game</p>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.button} onClick={trackGame}>Track game</button>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (trackGames){
+        return (
+            <div>
+                <div className={styles.titleContainer}>
+                    <button className={styles.backButton} onClick={trackGameBack}>Back</button>
+                    <div className={styles.title}>
+                        <PageTitle text='Overwatch Tools'/>
+                    </div>
+                </div>
+                <div className={styles.pageContent}>
+                    <p className={styles.text}>Select a role you would like to track games for:</p>
                     <div className={styles.roleIcons}>
                         <img src={tank} alt="tankIcon" className={styles.tankIcon} />
                         <img src={dps} alt='dpsIcon' className={styles.dpsIcon} />
