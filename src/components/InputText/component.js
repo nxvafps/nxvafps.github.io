57b1d3f97fdec35f4eb860c593e77518acb86929
nova <oliverquirk11@gmail.com>
2024-10-05 20:58:04 +0100

    text input component is now styled with styled components

--- src/components/InputText/component.js
+++ src/components/InputText/component.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
-const InputText = ({ value, onChange, placeholder='Enter Text', height = '40px', width = '200px' }) => {
+import StyledInput from "./styles";
+
+const InputText = ({ value, onChange, placeholder='Enter Text', height, width }) => {
     const hasValue = useState(!!value);
     return (
-        <input
+        <StyledInput
             type='text'
             className={`${styles.input} ${hasValue ? styles.hasValue : ''}`}
             value={value}
             onChange={onChange}
             placeholder={placeholder}
-            style={{ height: height, width: width}}
+            height={height}
+            width= {width}
         />
     )
 }
--- src/components/InputText/styles.js
+++ src/components/InputText/styles.js
@@ -1,23 +1,30 @@
-const styles = {
-    input: {
-        boxSizing: 'border-box',
-        textAlign: 'center',
-        border: 'none',
-        outline: 'none',
-        border: '1px solid transparent',
-        backgroundColor: '#7b7777',
-        color: '#ffffff',
-        '&::placeholder': {
-            color: '#ffffff',
-            fontSize: '15px',
-        },
-        '&:hover, &:focus, &:not(.hasValue)': {
-            border: '1px solid #ffffff',
-        },
-        '&.hasValue:not(:focus):not(:hover)': {
-            border: '1px solid transparent',
-        },
-    },
-};
+import styled from "styled-components";
 
-export default styles;
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  text-align: center;
+  border: none;
+  outline: none;
+  border: 1px solid transparent;
+  background-color: #7b7777;
+  color: #ffffff;
+  height: ${({ height }) => height || '40px'};
+  width: ${({ width }) => width || '200px'};
+
+  &::placeholder {
+    color: #ffffff;
+    font-size: 15px;
+  }
+
+  &:hover,
+  &:focus,
+  &:not(.hasValue) {
+    border: 1px solid #ffffff;
+  }
+
+  &.hasValue:not(:focus):not(:hover) {
+    border: 1px solid transparent;
+  }
+`;
+
+export default StyledInput;
