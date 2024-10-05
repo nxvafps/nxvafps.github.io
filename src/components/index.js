4735a65fa806528e0d6f157e73f24c7ee3109423
nova <oliverquirk11@gmail.com>
2024-10-05 20:25:44 +0100

    updated component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 export * from './Title';
 export * from './Subtitle';
+export * from './Button';
+export * from './InputDropdown';
+export * from './InputNumber';
+export * from './InputText';
+export * from './NavButton';
