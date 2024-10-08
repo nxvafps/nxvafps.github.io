74a3af0716da6f5aaf287fcca6629b6f3ec5c52d
nova <oliverquirk11@gmail.com>
2024-10-08 23:30:29 +0100

    styled tos page with styled components

---
+++ src/App/pages/TermsOfService/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TermsContainer = styled.div`
+  width: 80%;
+  margin: 30px auto 0 auto;
+`;
+
+export const Terms = styled.p`
+  color: #ffffff;
+  font-size: clamp(0.5rem, 2vw + 1rem, 1.5rem);
+`;
